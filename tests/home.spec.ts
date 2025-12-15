cc916d056bb63f2832d254f9b4bb024c01aa9ff9
mnedza <mnedza@mnedza.local>
2025-12-15 16:47:49 +0100

    added empty structure - test.step for check first item details

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,7 +1,40 @@
 import { test, expect } from "@playwright/test";
 import { HomePage } from "../pages/HomePage";
 
-test("Check first item details", async ({ page }) => {});
+test("Check first item details", async ({ page }) => {
+  const homePage = new HomePage(page);
+  await test.step("Go to items list view - inventory page", async () => {
+    await page.goto("/inventory.html");
+    await expect(page).toHaveURL(/.*inventory\.html/);
+  });
+
+  await test.step("image is visible - list view", async () => {});
+
+  await test.step("image is correct - list view", async () => {});
+
+  await test.step("Item name is correct - list view", async () => {});
+
+  await test.step("Item description is correct - list view", async () => {});
+
+  await test.step("Item price is correct - list view", async () => {});
+
+  await test.step("Item button 'add to cart' is visible - list view", async () => {});
+
+  await test.step("Go to item details", async () => {});
+
+  await test.step("image is visible - item details view", async () => {});
+
+  await test.step("image is correct - item details view", async () => {});
+
+  await test.step("Item name is correct - item details view", async () => {});
+
+  await test.step("Item description is correct - item details view", async () => {});
+
+  await test.step("Item price is correct - item details view", async () => {});
+
+  await test.step("Item button 'add to cart' is visible - item details view", async () => {});
+});
+
 test("Check every item details", async ({ page }) => {});
 test("Check last item details", async ({ page }) => {});
 
