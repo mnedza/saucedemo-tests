0358444cf92588505fbd0aece41cd375d82eebb3
mnedza <mnedza@mnedza.local>
2025-12-15 16:30:36 +0100

    added logout test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,3 +4,16 @@ import { HomePage } from "../pages/HomePage";
 test("Check first item details", async ({ page }) => {});
 test("Check every item details", async ({ page }) => {});
 test("Check last item details", async ({ page }) => {});
+
+test("logout from the app", async ({ page }) => {
+  const homePage = new HomePage(page);
+  await test.step("Go to main page", async () => {
+    await page.goto("/inventory.html");
+    await expect(page).toHaveURL(/.*inventory\.html/);
+  });
+
+  await test.step("Logout from the app", async () => {
+    await homePage.logout();
+    await expect(page).toHaveURL("/");
+  });
+});
