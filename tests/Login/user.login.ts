149216f4020880a4661abbe26f33ff54496985fa
Marcel <marcelnedza2000@gmail.com>
2025-12-26 20:37:26 +0100

    Added cases for login tests - no username, no password, locked out user

--- tests/Login/user.login.ts
+++ tests/Login/user.login.ts
@@ -2,31 +2,51 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../pages/LoginPage";
 import { attemptLogin } from "../../utils/login";
 
-test.describe("Standard user", () => {
-  test("User can log in with valid credentials", async ({ page }) => {
-    await attemptLogin(page, "standard_user", "secret_sauce");
-    await expect(page).toHaveURL(/inventory.html/);
-  });
+test("User can log in with valid credentials", async ({ page }) => {
+  await attemptLogin(page, "standard_user", "secret_sauce");
+  await expect(page).toHaveURL(/inventory.html/);
 });
 
-test.describe("Invalid credentials", () => {
-  test("User cannot log in and sees error message", async ({ page }) => {
-    const login = new LoginPage(page);
-    attemptLogin(page, "incorrect_credentials", "incorrect_credentials");
-    await expect(page).not.toHaveURL(/inventory.html/);
-    await expect(login.expectedError).toHaveText(
-      "Epic sadface: Username and password do not match any user in this service"
-    );
-  });
+test("User cannot log in with invalid credentials and sees error message", async ({
+  page,
+}) => {
+  const login = new LoginPage(page);
+  attemptLogin(page, "incorrect_credentials", "incorrect_credentials");
+  await expect(page).not.toHaveURL(/inventory.html/);
+  await expect(login.expectedError).toHaveText(
+    "Epic sadface: Username and password do not match any user in this service"
+  );
 });
 
-test.describe("Locked out user", () => {
-  test("User cannot log in and sees error message", async ({ page }) => {
-    const login = new LoginPage(page);
-    attemptLogin(page, "locked_out_user", "secret_sauce");
-    await expect(page).not.toHaveURL(/inventory.html/);
-    await expect(login.expectedError).toHaveText(
-      "Epic sadface: Sorry, this user has been locked out."
-    );
-  });
+test("User cannot log in with empty username and sees error message", async ({
+  page,
+}) => {
+  const login = new LoginPage(page);
+  attemptLogin(page, "", "incorrect_credentials");
+  await expect(page).not.toHaveURL(/inventory.html/);
+  await expect(login.expectedError).toHaveText(
+    "Epic sadface: Username is required"
+  );
+});
+
+test("User cannot log in with empty password and sees error message", async ({
+  page,
+}) => {
+  const login = new LoginPage(page);
+  attemptLogin(page, "incorrect_credentials", "");
+  await expect(page).not.toHaveURL(/inventory.html/);
+  await expect(login.expectedError).toHaveText(
+    "Epic sadface: Password is required"
+  );
+});
+
+test("Locked out user cannot log in and sees error message", async ({
+  page,
+}) => {
+  const login = new LoginPage(page);
+  attemptLogin(page, "locked_out_user", "secret_sauce");
+  await expect(page).not.toHaveURL(/inventory.html/);
+  await expect(login.expectedError).toHaveText(
+    "Epic sadface: Sorry, this user has been locked out."
+  );
 });
